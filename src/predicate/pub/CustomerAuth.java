7eebb0738f843fb8df0f09a5644cff81de1137c8
Saurabh Singh <saurabhbans87@gmail.com>
2023-11-11 16:25:42 +0530

    adding prediacte case

--- src/predicate/pub/CustomerAuth.java
+++ src/predicate/pub/CustomerAuth.java
@@ -8,13 +8,14 @@ package predicate.pub;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class CustomerAuth {
     public static void main(String[] args) {
         List<Customer> customerList = new ArrayList<>();
         customerList.add(new Customer("Ravindra",18,true));
         customerList.add(new Customer("Arun",19,true));
-        customerList.add(new Customer("Mahesh",17,true));
+        customerList.add(new Customer("Mahesh",17,false));
         customerList.add(new Customer("Mohan",20,false));
         Predicate<Customer> customerPredicate = customer -> customer.getCouple().equals(Boolean.TRUE)&&customer.getAge()>=18;
         System.out.println("Allowed customer are : " );
@@ -23,5 +24,10 @@ public class CustomerAuth {
                 System.out.println(customer);
             }
         }
+        System.out.println("Not allowed customer are : " );
+        Predicate<Customer> customerPredicate1 = customer -> !customer.isCouple && customer.getAge()>=18;
+        List<Customer> notAllowedCustomer = customerList.stream().filter(customerPredicate1).collect(Collectors.toList());
+        System.out.println(notAllowedCustomer);
+
     }
 }
--- src/predicate/userauth/UserAuth.java
+++ src/predicate/userauth/UserAuth.java
@@ -16,11 +16,15 @@ public class UserAuth {
         userList.add(new User("Test","Test"));
         userList.add(new User("User","Password"));
         Predicate<User> userPredicate = user -> user.getUserName().equals("User") && user.getPassword().equals("Password");
-        System.out.println("Valid credential are:");
+        System.out.println("Valid user credential are:");
         for (User user:userList) {
             if(userPredicate.test(user)){
                 System.out.println(user);
             }
         }
+        System.out.println("Valid user credential are:");
+        Predicate<User> userPredicate1 = user -> user.getUserName().equals("Test")&&user.getPassword().equals("Test");
+        List<User> authUser = userList.stream().filter(userPredicate1).collect(Collectors.toList());
+        System.out.println(authUser);
     }
 }
