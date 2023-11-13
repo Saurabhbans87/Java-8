e047f5f759813694c5dbcf225b07dcf608319cd5
Saurabh Singh <saurabhbans87@gmail.com>
2023-11-14 00:32:15 +0530

    update random number

--- src/supplier/RandomNumberGenerator.java
+++ src/supplier/RandomNumberGenerator.java
@@ -12,6 +12,18 @@ public class RandomNumberGenerator {
         // range is from 1 to 50
         int rand = obj1.randomNumberWithRange(1, 50);
         System.out.println(rand);
+        System.out.println("******************Example*****************");
+        double randomeNumber1 = Math.random();
+        System.out.println(randomeNumber1);
+        System.out.println("******************Example*****************");
+        double randomeNumber2 = Math.random();
+        System.out.println(randomeNumber2);
+        System.out.println("******************Random number 1 to 5*****************");
+        double randomeNumber3 = Math.random()*5;
+        int randomeNumber4 = (int) (Math.random()*5);
+        System.out.println(randomeNumber3);
+        System.out.println(randomeNumber4);
+
     }
     //defining method for a random number generator
     int randomNumberWithRange(int min, int max) {
