cc1659b2f01f382e79b415ba7f7861f539956c58
Saurabh Singh <saurabhbans87@gmail.com>
2024-02-09 20:15:41 +0530

    updated foreach

---
+++ src/example/CollectorClassExample.java
@@ -0,0 +1,22 @@
+/**
+ * Create by saurabh
+ * Date: 16/01/24
+ * Project Name: Java-8
+ */
+package example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/*
+The Collectors class in Java 8 is a utility class in the java.util.stream package.
+Collectors class provides a set of static factory methods for creating collectors.
+Collectors are used with the collect operation of streams to transform the elements
+of a stream into a different collection like a List, Set, or Map.
+ */
+public class CollectorClassExample {
+    public static void main(String[] args) {
+        List<String> stringList = Arrays.asList("Mumbai","Kolkata","Chennai","Delhi","Lucknow");
+    }
+}
