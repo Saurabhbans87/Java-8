d9b6ef1000d50fddb6bd8ced2db41d744c5db4c0
Saurabh Singh <saurabhbans87@gmail.com>
2024-03-13 23:39:01 +0530

    updated foreach

---
+++ src/MapExample.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+/**
+ * Create by saurabh
+ * Date: 26/02/24
+ * Project Name: Java-8
+ */
+
+public class MapExample {
+    public static void main(String[] args) {
+        HashMap<String,Integer> hashMap = new HashMap();
+    }
+}
