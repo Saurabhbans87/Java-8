1e2317893dd3607aa44b3bde1d1816860dd2c71e
Saurabh Singh <saurabhbans87@gmail.com>
2024-04-03 18:10:21 +0530

    navigable map

--- src/example/NavigableExample.java
+++ src/example/NavigableExample.java
@@ -20,28 +20,39 @@ public class NavigableExample {
         navigableMap.put(7,"Seven");
         navigableMap.put(8,"Eight");
         navigableMap.put(9,"Nine");
-
+        System.out.println("************************************descendingMap*********************************");
         System.out.println("descendingMap" + navigableMap.descendingMap());
-
+        System.out.println("************************************descendingKeyMap*********************************");
         System.out.println("descendingKeyMap" + navigableMap.descendingKeySet());
 
+        System.out.println("************************************ceilingEntry*********************************");
         System.out.println("ceilingEntry" + navigableMap.ceilingEntry(3));
+        System.out.println("************************************ceilingKey*********************************");
         System.out.println("ceilingKey" + navigableMap.ceilingKey(4));
 
+        System.out.println("************************************floorEntry*********************************");
         System.out.println("floorEntry" + navigableMap.floorEntry(3));
+        System.out.println("************************************floorKey*********************************");
         System.out.println("floorKey" + navigableMap.floorKey(4));
 
+        System.out.println("************************************higherEntry*********************************");
         System.out.println("higherEntry" + navigableMap.higherEntry(3));
+        System.out.println("************************************higherKey*********************************");
         System.out.println("higherKey" + navigableMap.higherKey(4));
 
+        System.out.println("************************************lowerEntry*********************************");
         System.out.println("lowerEntry" + navigableMap.lowerEntry(3));
+        System.out.println("************************************lowerKey*********************************");
         System.out.println("lowerKey" + navigableMap.lowerKey(4));
 
+        System.out.println("************************************firstEntry*********************************");
         System.out.println("firstEntry" + navigableMap.firstEntry());
+        System.out.println("************************************lastEntry*********************************");
         System.out.println("lastEntry" + navigableMap.lastEntry());
 
+        System.out.println("************************************pollFirstEntry*********************************");
         System.out.println("pollFirstEntry" + navigableMap.pollFirstEntry());
+        System.out.println("************************************pollLastEntry*********************************");
         System.out.println("pollLastEntry" + navigableMap.pollLastEntry());
-
     }
 }
